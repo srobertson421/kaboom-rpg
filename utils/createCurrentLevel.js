facc5bf3e5f31114fff882ecd80f7e08f93a06dc
srobertson421 <srobertson421@gmail.com>
2021-04-19 19:12:40 -0700

    added paths to levels

--- utils/createCurrentLevel.js
+++ utils/createCurrentLevel.js
@@ -1,7 +1,8 @@
 import {
   addLevel,
   vec2,
-  sprite
+  sprite,
+  rand
 } from '../engine.js';
 
 import levels from '../levels.js';
@@ -12,13 +13,21 @@ export default function createCurrentLevel() {
     width: 16,
     height: 16,
     pos: vec2(0, 0),
-    'g': [
-      sprite('tileSheet', { frame: 11 }),
-      'grass'
-    ],
+    'g': () => {
+      const tileNum = Math.round(rand(64,65));
+
+      return [
+        sprite('tileSheet', { frame: tileNum }),
+        'grass'
+      ]
+    },
     'f': [
       sprite('tileSheet', { frame: 12 }),
       'flower'
+    ],
+    'p': [
+      sprite('tileSheet', { frame: 10 }),
+      'path'
     ]
   });
 
