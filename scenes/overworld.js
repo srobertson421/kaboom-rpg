63af780e20aeb5ce35165ca2c171bbc394da9228
derek <derek@crowdhubapps.com>
2021-04-22 16:53:42 -0700

    assests

--- scenes/overworld.js
+++ scenes/overworld.js
@@ -1,7 +1,11 @@
 import {
   scene,
   gravity,
-  camScale
+  camScale,
+  add,
+  sprite,
+  pos,
+  get
 } from '../engine.js';
 import { overworldMusic } from '../state/music.js';
 import SCALE from '../state/scale.js'
@@ -13,6 +17,23 @@ import {
   playerControls
 } from '../entities/player.js';
 import createCurrentLevel from '../utils/createCurrentLevel.js';
+
+
+function addFire() {
+  return add([
+    sprite('fire', { animSpeed: 0.25 }),
+    pos(150, 100),
+    'fire'
+  ]);
+}
+
+function addMentor(){
+  return add([
+    sprite('mentor', {animSpeed: 0.25}),
+    pos(230, 100),
+    'mentor'
+  ])
+}
 
 const overworldScene = () => {
   return scene('overworld', () => {
@@ -27,7 +48,11 @@ const overworldScene = () => {
     camScale(SCALE.value);
   
     createCurrentLevel();
+
+    addFire();
   
+    addMentor();
+
     addPlayer();
   
     playerControls();
@@ -35,6 +60,66 @@ const overworldScene = () => {
     playerCollisions();
 
     playerActions();
+
+    const fire = get('fire')[0];
+    fire.play('burn');
+
+    var mentor = get('mentor')[0];
+    const MENTOR_SPEED = 75;
+
+    function mentorStop(){
+      mentor.stop();
+      if(mentor.direction === 'down'){
+        mentor.frame = 0;
+      } else {
+        mentor.frame = 3;
+      }
+
+      
+    }
+
+    function walkDown(){
+      mentor.move(0, MENTOR_SPEED);
+      mentor.play('walkDown');
+    }
+
+    function walkUp(){
+      mentor.move(0, -MENTOR_SPEED);
+      mentor.play('walkUp');
+    }
+
+    function mentorWalk(){
+
+      clearInterval(walkDown);
+      clearInterval(walkUp);
+
+      if(mentor.direction){
+
+        if(mentor.direction === 'down'){
+
+          mentor.direction = 'up';
+          setInterval(walkUp, 100);
+          
+
+        } else {
+          mentor.direction = 'down';
+          setInterval(walkDown, 100);
+        }
+
+      } else {
+
+        mentor.direction = 'down';
+        setInterval(walkDown, 100);
+
+      }
+
+      setTimeout(mentorStop, 1500);
+
+    }
+
+    // setInterval(function(){
+    //   mentorWalk();
+    // }, 2000)
 
     // there's no spatial hashing yet, if too many blocks causing lag, consider hard disabling collision resolution from blocks far away by turning off 'solid'
     // action("flower", (b) => {
